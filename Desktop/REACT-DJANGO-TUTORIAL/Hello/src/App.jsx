92b65bf50d2dc366d6a4c2ceeab4bb9eb0e2b869
Geno Ureta <genolauzureta@gmail.com>
2023-03-03 22:23:10 +0800

    added modal and styled form

--- Desktop/REACT-DJANGO-TUTORIAL/Hello/src/App.jsx
+++ Desktop/REACT-DJANGO-TUTORIAL/Hello/src/App.jsx
@@ -1,9 +1,45 @@
 import { useState } from 'react'
 import Employee from './components/Employee'
-
+import { v4 as uuidv4 } from 'uuid';
 
 function App() {
   const [role, setRole] = useState('dev');
+  const [employees, setEmployees] = useState(
+    [
+      {
+        name:"Caleb",
+        role: "Developer",
+        img: 'https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg'
+      },
+      {
+        name:"Haru",
+        role: "Developer",
+        img: 'https://images.pexels.com/photos/3536436/pexels-photo-3536436.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load'
+      },
+      {
+        name:"Jonny",
+        role: "Developer",
+        img: 'https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg'
+      },
+
+      {
+        name:"Luckas",
+        role: "Astronaut",
+        img: 'https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg'
+      },
+
+      {
+        name:"Mike",
+        role: "Data Scientist",
+        img: 'https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg'
+      },
+      {
+        name:"Shain",
+        role: "Business Analyst",
+        img: 'https://images.pexels.com/photos/774909/pexels-photo-774909.jpeg?auto=compress&cs=tinysrgb&w=1600'
+      },
+    ]
+  )
   const showEmployees = true;
 
   return (
@@ -16,9 +52,19 @@ function App() {
           }} 
         
           />
-            <Employee name="Caleb" role='Developer'/>
-            <Employee name="Maria" role={role} />
-            <Employee name="Carl" />
+           <div className='flex flex-wrap justify-center'>
+              {employees.map((employee) => {
+                  console.log(uuidv4())
+
+                return (
+                  <Employee 
+                    key={uuidv4()} 
+                    name={employee.name}
+                    role={employee.role} 
+                    img={employee.img} />
+                )
+              })}
+           </div>
         </>
         ) : (
           <p>You cannot see the employees</p>
